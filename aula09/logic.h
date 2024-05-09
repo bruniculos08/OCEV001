47f0a6caa8353be421cfb12e8df4deb4c6ed5641
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2024-05-09 12:23:39 -0300

    update

--- aula09/logic.h
+++ aula09/logic.h
@@ -1,22 +1,35 @@
 #include <bits/stdc++.h>
-// #include <gnuplot-iostream.h>
+#include <gnuplot-iostream.h>
 
 using namespace std;
 
-#define POPULATION_SIZE 30
-#define MUTATION_PROBABILITY 0.001
+#define POPULATION_SIZE 10
+#define MUTATION_PROBABILITY 0.05
 //  Se o seguinte valor for 0 ou maior que a dimensão dos individuos - 2, 
 //  faz-se crossover uniforme:
-#define CUT_POINTS_CROSSOVER 2 
+#define CUT_POINTS_CROSSOVER 2
 //  Se este valor for 0 ou maior que POPULATION_SIZE, 
 //  todos os individuos participam do torneio:
-#define TOURNAMENT_SAMPL 0 
-#define GENERATIONS 1000
-//  Se o seguinte valor for true será feita seleção por torneio, 
-//  e se for false será feita seleção por roleta viciada:
-#define SELECTION_OPT false 
-
-union allele
+#define TOURNAMENT_SAMPL 5
+#define GENERATIONS 20000
+//  Se o seguinte valor for "true" será feita seleção por torneio, 
+//  e se for "false" será feita seleção por roleta viciada:
+#define SELECTION_OPT false
+//  Se o seguinte valor for true será feito crossover PMX, e se
+//  for "false" será feita crossover CX:
+#define CROSSOVER_OPT false
+//  Coeficiente de relação entre o fitness escalonado máximo e o
+//  fitness escalonado médio:
+#define SCALING_CONSTANT 1.2
+// 6 de 20 (150 gerações, 4 pontos de corte (crossover PMX), torneio com 5 amostras, taxa de mutação = 0.01, população = 100 e C = 1.5)
+// 2 de 5 (150 gerações, 4 pontos de corte (crossover PMX), torneio com 5 amostras, taxa de mutação = 0.02, população = 100 e C = 1.8)
+// 2 de 5 (150 gerações, 4 pontos de corte (crossover PMX), torneio com 5 amostras, taxa de mutação = 0.02, população = 100 e C = 1.2)
+// ? de ? (150 gerações, 2 pontos de corte (crossover PMX), roleta, taxa de mutação = 0.02, população = 100 e C = 1.2)
+// 2 de 5 (150 gerações, crossover CX, roleta, taxa de mutação = 0.00001, população = 100 e C = 1.2)
+// 0 de 2 (150 gerações, crossover CX, torneio com 15 amostras, taxa de mutação = 0.00001, população = 100 e C = 1.2)
+// 8 de 20 (150 gerações, crossover CX, torneio com 15 amostras, taxa de mutação = 0.001, população = 100 e C = 1.2)
+// 0 de 0 (150 gerações, crossover CX, torneio com 15 amostras, taxa de mutação = 0.001, população = 100 e C = 1.2)
+union allele 
 {
     int integer;
     bool boolean;
@@ -27,6 +40,7 @@ typedef struct Population
 {
     vector<vector<allele>> matrix;
     int ind_dim, pop_size;
+    double alpha, beta;
 } population;
 
 typedef struct NQueens
@@ -48,6 +62,7 @@ template <typename T> void eraseFast(vector<T> &v, int index);
 template <typename T> T getRandom(vector<T> &v);
 void generateGraph();
 double randomDouble(double max, double min);
+int linearSearch(vector<allele> &v, allele e, int start, int end);
 
 //  Funções para gerar população:
 void generateIntPop(int ind_dim, int pop_size, allele low, allele high, vector<vector<allele>> &v);
@@ -66,28 +81,42 @@ int evolve(nqueens &b, population &p);
 //  - Trocar a velha geração pela nova geração:
 void changeGeneration(vector<allele> *children, population &p);
 //  - Printar tabuleiro:
-void printBoard(nqueens &b);
+void printBoard(nqueens &b, vector<allele> &ind);
 //  - Função Fitness: para SAT (avalia o número de clausulas satisfeitas por um indivíduo).
 int fitness(nqueens &b, int ind_id, population &p);
 //  - Torneio: selecionar n aleatórios e retornar o melhor.
 int tournament(nqueens &b, int samples_num, population &p, vector<int> &single_ind);
 //  - Roleta viciada:
-int roulette(vector<int> &single_ind, nqueens &sat, population &p);
+int roulette(vector<int> &single_ind, nqueens &b, population &p);
 //  - Realiza crossover entre todos:
-void crossoverAll(nqueens &b, int samples_num, population &p, bool genetic_operator);
+void crossoverAll(nqueens &b, int samples_num, population &p, int cuts_num);
 //  - Cruzamento: misturar 2 individuos gerando 2 filhos usando método PMX.
 void crossoverPMX(int ind1, int ind2, population &p, int cuts_num);
 //  - Cruzamento: misturar 2 individuos gerando 2 filhos usando método CX.
 void crossoverCX(int ind1, int ind2, population &p, int cuts_num);
 //  - Mutação: alterar randomicamente alelos de um indivíduo.
 void mutation(vector<allele> &dna, population &p);
+//  - Mutação: alterar randomicamente alelos de um indivíduo.
+void mutateAllele(vector<allele> &dna, population &p, int allele_index, vector<int> &poll);
 //  - Pegar o melhor indivíduo da geração:
 int getElite(nqueens &b, population &p, vector<allele> &elite_place);
 //  - Pega o id do pior indivíduo:
 int getWorst(nqueens &b, population &p);
 //  - Calcula a posição equivalente de um número no tabuleiro
-pair<int, int> getPosition(nqueens &b, int id);
+pair<int, int> getPosition(nqueens &b, allele queen, vector<allele> &v);
 //  - Verifica o número de conflitos nas diagonais de uma rainha:
 int lookDiag(vector<allele> &ind, nqueens &b, allele actual_queen);
 //  - Verifica o número de conflitos na colunas e na linha de uma rainha:
 int lookRowAndCol(vector<allele> &ind, nqueens &b, allele actual_queen);
+
+// Funções para implementação de escalonamento linear:
+//  - Média da função fitness:
+double fitnessAvg(nqueens &b, population &p);
+//  - Valor máximo da função fitness:
+double fitnessMin(nqueens &b, population &p);
+//  - Valor mínimo da função fitness:
+double fitnessMax(nqueens &b, population &p);
+//  - Calculo dos valores alpha e beta:
+void calcEsc(nqueens &b, population &p, double c);
+//  - Função fitness com escalonamento:
+double fitnessEsc(nqueens &b, int ind_id, population &p);
--- aula09/perm.cpp
+++ aula09/perm.cpp
@@ -2,28 +2,30 @@
 
 int main(void)
 {
-    // srandom(time(0));
+    srandom(time(0));
 
     //  Criar o tabuleiro:
     nqueens b;
-    b.dim = 8;
-    b.board = createBoard(8, 8);
+    b.dim = 32;
+    b.board = createBoard(b.dim, b.dim);
 
     //  Gerar a população:
     allele low, high;
     low.integer = 0;
-    high.integer = b.dim * b.dim;
+    high.integer = b.dim;
     population p;
-    generatePermPop(b.dim, 10, low, high, p.matrix);
+    p.ind_dim = b.dim;
+    p.pop_size = POPULATION_SIZE;
+    generatePermPop(b.dim, p.pop_size, low, high, p.matrix);
 
-    cout << "Fitness do individuo " << 1 << " = " << fitness(b, 1, p) << endl;
-    for(allele &a : p.matrix[1]){
-        pair<int,int> u = getPosition(b, a.integer);
-        cout << "("<< u.first << ", " << u.second << ")" << " ";
-    }
-    cout << endl;
+    int result = evolve(b, p);
+    cout << "best ind = " << result << endl;
+    cout << "best ind fitness = " << fitness(b, result, p) << endl;
+    generateGraph();
 
-    printIntMatrix(p.matrix);
+    // printIntMatrix(p.matrix);
+    cout << "Resultado: " << endl;
+    printBoard(b, p.matrix[result]);
     return 0;
 }
 
@@ -51,8 +53,8 @@ void generatePermPop(int ind_dim, int pop_size, allele low, allele high, vector<
     vector<allele> line(ind_dim, low);
     vector<vector<allele>> matrix(pop_size, line);
     
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    // std::random_device rd;
+    // std::mt19937 gen(rd());
     
     vector<int> poll(high.integer - low.integer), temp_poll;
     for(int i = 0; i < (int) poll.size(); i++) poll[i] = low.integer + i;
@@ -67,7 +69,8 @@ void generatePermPop(int ind_dim, int pop_size, allele low, allele high, vector<
             matrix[y][x].integer = temp_poll[rand_index];
             eraseFast(temp_poll, rand_index);
         }
-        shuffle(begin(matrix[y]), end(matrix[y]),  gen);
+        // shuffle(begin(matrix[y]), end(matrix[y]),  gen);
+        permute(matrix[y]);
     }
     v.swap(matrix);
 }
@@ -106,9 +109,102 @@ template <typename T> T getRandom(vector<T> &v)
     return item;
 }
 
+void generateGraph()
+{
+    Gnuplot gp("gnuplot");
+    gp << "set title 'Gráfico de convergência com " << GENERATIONS  << " gerações'\n";
+    gp << "set terminal 'svg'\n";
+    // gp << "set output \"GraphicFiles/convergencia\"\n";
+    gp << "set output \"convergencia.svg\"\n";
+
+    gp << "set xlabel \"geração\"\n";
+    gp << "set ylabel \"fitness do melhor individuo\"\n";
+    gp << "set grid\n";
+
+    gp << "plot '-' with lines title 'points'\n";
+    gp.send1d(data_plot);
+}
+
 double randomDouble(double max, double min)
 {
     return (((double) random()) / ((double) RAND_MAX)) * (max - min) + min;
+}
+
+int linearSearch(vector<allele> &v, allele e, int start, int end)
+{
+    assert((size_t) end < v.size());
+    for (int i = start; i <= end; i++) if(v[i].integer == e.integer) return i;
+    return -1;
+}
+
+/* ----------------- Funções para modelagem do problema N-Rainhas --------------- */
+
+int **createBoard(int width, int height)
+{
+    int **matrix;
+    matrix = new int*[height];
+    for(int i = 0; i < height; i++)
+    {
+        matrix[i] = new int[width];
+        for(int j = 0; j < width; j++)
+            matrix[i][j] = 0;
+    }
+    return matrix;
+}
+
+int evolve(nqueens &b, population &p)
+{
+    calcEsc(b, p, SCALING_CONSTANT);
+    // double best_fitness = 0.0;
+    int best_id = 0, worst_id = 0, counter = GENERATIONS;
+    vector<allele> best_individual;
+    best_id = getElite(b, p, best_individual);
+    cout << "Started evolving" << endl;
+    do
+    {
+        calcEsc(b, p, SCALING_CONSTANT);
+        crossoverAll(b, p.pop_size, p, CUT_POINTS_CROSSOVER);
+        worst_id = getWorst(b, p);
+        // cout << fitnessEsc(b, worst_id, p) << " <- old (worst) result" << endl;
+        if(counter % 200 == 0) 
+            cout << fitness(b, worst_id, p) << " <- old (worst) result" << endl;
+        p.matrix[worst_id] = best_individual;
+
+
+        best_id = getElite(b, p, best_individual);
+        // best_fitness = fitnessEsc(b, best_id, p); 
+
+        // cout << best_fitness << " <- current best result" << endl;
+        if(counter % 200 == 0)
+            cout << fitness(b, best_id, p) << " <- current best result" << endl;
+        
+        if(GENERATIONS > 0) counter--;
+        data_plot.push_back(make_pair((double) (GENERATIONS - counter), (double) fitness(b, best_id, p)));
+
+    } while (fitness(b, best_id, p) != (b.dim * (b.dim - 1)) && counter != 0);
+    return best_id;
+}
+
+void changeGeneration(vector<vector<allele>> &children, population &p)
+{
+    for(int i = 0; i < p.pop_size; i++)
+        p.matrix[i] = children[i];
+    children.clear();
+}
+
+void printBoard(nqueens &b, vector<allele> &ind)
+{
+    b.board = createBoard(b.dim, b.dim);
+    for(allele &queen : ind)
+    {
+        pair<int, int> pos = getPosition(b, queen, ind);
+        b.board[pos.first][pos.second] = 1;
+    }
+    for(int i = 0; i < b.dim; i++)
+    { 
+        for(int j = 0; j < b.dim; j++) cout << b.board[i][j] << " ";
+        cout << endl;
+    }
 }
 
 int fitness(nqueens &b, int ind_id, population &p)
@@ -117,34 +213,304 @@ int fitness(nqueens &b, int ind_id, population &p)
     pieces = p.matrix[ind_id];
 
     int conflicts = 0;
-    for(allele &queen : pieces)
-        conflicts += lookRowAndCol(pieces, b, queen) + lookDiag(pieces, b, queen);
-
+    for(allele &queen : pieces){
+        // conflicts += lookRowAndCol(pieces, b, queen);
+        conflicts += lookDiag(pieces, b, queen);
+    }
     return (b.dim * (b.dim - 1)) - conflicts;
 }
 
-int **createBoard(int width, int height)
+int tournament(nqueens &b, int samples_num, population &p, vector<int> &single_ind)
 {
-    int **matrix;
-    matrix = new int*[height];
-    for(int j = 0; j < height; j++)
+    vector<int> poll_rest;
+    int choosen_ind_id = -1, rand_id;
+    double best_fitness = -1.0;
+    if(samples_num <= 0 || samples_num >= (int) single_ind.size()) 
+        samples_num = single_ind.size();
+
+    for(int i = 0; i < samples_num; i++)
     {
-        matrix[j] = new int[width];
+        rand_id = getRandom<int>(single_ind);
+        if(choosen_ind_id == -1 || fitnessEsc(b, rand_id, p) > best_fitness)
+        {
+            if(choosen_ind_id != -1) poll_rest.push_back(choosen_ind_id);
+            choosen_ind_id = rand_id;        
+            best_fitness = fitnessEsc(b, choosen_ind_id, p);
+        }
+        else poll_rest.push_back(rand_id);
     }
-    return matrix;
+
+    for(int id : poll_rest) single_ind.push_back(id);
+    poll_rest.clear(); 
+    return choosen_ind_id;
 }
 
-pair<int, int> getPosition(nqueens &b, int id)
+int roulette(vector<int> &single_ind, nqueens &b, population &p)
 {
-    int row = (int) floor((double) id / (double) b.dim);
-    int col = id % b.dim;
+    double summ = 0.0;
+    // double f_min = fitnessMin(b, p);
+    // if(f_min < 0)
+    //     for(int index : single_ind){
+    //         summ += (double) fitnessEsc(b, index, p) + abs(f_min);
+    //     }
+    // else
+    for(int index : single_ind){
+        summ += (double) fitnessEsc(b, index, p);
+    }
+
+    if(summ == 0){
+        int rand_id = random() % single_ind.size();
+        int result = single_ind[rand_id];
+        eraseFast(single_ind, rand_id);
+        return rand_id;
+    }
+
+    double prob; 
+    vector<double> roulette_chances;
+    for(int index : single_ind)
+    {
+        prob = 100.0 * fitnessEsc(b, index, p) / summ;
+        roulette_chances.push_back(prob);
+    }
+
+
+    double winner = 100.0 * (double) ((double) random() / (double) RAND_MAX);
+    double acc = 0.0;
+    int result = 0;
+    for(int i = 0; i < (int) roulette_chances.size(); i++)
+    {
+        if(acc <= winner && winner <= acc + roulette_chances[i])
+        {
+            result = single_ind[i];
+            eraseFast(single_ind, i);
+            // cout << "Individuo selecionado pela roleta = " 
+            //      << single_ind[i] << " (fitness = " << fitness(sat, result, p) << ")" << endl;
+            return result;
+        }
+        acc += roulette_chances[i];
+    }
+    return 0;
+}
+
+void crossoverAll(nqueens &b, int samples_num, population &p, int cuts_num)
+{
+    vector<int> single_ind;
+    for(int i = 0; i < p.pop_size; i++) single_ind.push_back(i);
+
+    int parent1_index, parent2_index;
+    while(single_ind.size() >= 2)
+    { 
+        if(SELECTION_OPT == true)
+        {
+            parent1_index = tournament(b, samples_num, p, single_ind);
+            parent2_index = tournament(b, samples_num, p, single_ind);
+        }
+        else
+        {
+            parent1_index = roulette(single_ind, b, p);
+            parent2_index = roulette(single_ind, b, p);
+        }
+        // cout << "crossing " << parent1_index << " and " << parent2_index << endl;
+        if(CROSSOVER_OPT)
+            crossoverPMX(parent1_index, parent2_index, p, cuts_num);
+        else
+            crossoverCX(parent1_index, parent2_index, p, cuts_num);
+        mutation(p.matrix[parent1_index], p);
+        mutation(p.matrix[parent2_index], p);
+    }
+}
+
+void crossoverPMX(int ind1, int ind2, population &p, int cut_size)
+{
+    allele none;
+    none.integer = -1;
+    vector<allele> f1 = p.matrix[ind1];
+    vector<allele> f2 = p.matrix[ind2];
+    vector<allele> s1(p.ind_dim, none);
+    vector<allele> s2(p.ind_dim, none);
+
+    int cut1 = random() % p.ind_dim, cut2 = random() % p.ind_dim;
+    if(cut_size > 0) cut2 = min(cut1 + cut_size - 1, p.ind_dim - 1);
+
+    // Colocando segmentos nos filhos:
+    for(int i = cut1; i <= cut2; i++)
+    {
+        // (1) Copiar segmento do primeiro pai para o primeiro filho:
+        s1[i] = f1[i];
+        // (2) Copiar segmento do segundo pai para o segundo filho:
+        s2[i] = f2[i];
+    }
+    // Colocando alelos por mapeamento:
+    for(int i = cut1; i <= cut2; i++)
+    {
+        // (1) Mapeamento do segundo pai para o primeiro filho:
+        if(linearSearch(s1, f2[i], cut1, cut2) == -1)
+        {
+            allele n = s1[i];
+            allele m = f2[i];
+            int n_pos_f2 = linearSearch(f2, n, 0, f2.size() - 1);
+            if(s1[n_pos_f2].integer == -1)
+                s1[n_pos_f2] = m;
+            else{
+                do{
+                    allele k = s1[n_pos_f2];
+                    int k_pos_f2 = linearSearch(f2, k, 0, f2.size() - 1);
+                    if(s1[k_pos_f2].integer == -1)
+                    {
+                        s1[k_pos_f2] = m;
+                        break;
+                    }
+                    n_pos_f2 = k_pos_f2;
+                } while (true);
+            }
+        }
+        // (2) Mapeamento do primeiro pai para o segundo filho:
+        if(linearSearch(s2, f1[i], cut1, cut2) == -1)
+        {
+            allele n = s2[i];
+            allele m = f1[i];
+            int n_pos_f1 = linearSearch(f1, n, 0, f1.size() - 1);
+            if(s2[n_pos_f1].integer == -1)
+                s2[n_pos_f1] = m;
+            else{
+                do{
+                    allele k = s2[n_pos_f1];
+                    int k_pos_f1 = linearSearch(f1, k, 0, f1.size() - 1);
+                    if(s2[k_pos_f1].integer == -1)
+                    {
+                        s2[k_pos_f1] = m;
+                        break;
+                    }
+                    n_pos_f1 = k_pos_f1;
+                } while (true);
+            }
+        }
+    } 
+
+    // (3) Completar os vetores:
+    int index_s1 = 0, index_s2 = 0;
+    for (int i = 0; i < p.ind_dim; i++)
+    {
+        if(linearSearch(s1, f2[i], 0, p.ind_dim - 1) == -1)
+        {
+            index_s1 = linearSearch(s1, none, 0, p.ind_dim - 1);
+            s1[index_s1] = f2[i];
+        }
+        if(linearSearch(s2, f1[i], 0, p.ind_dim - 1) == -1)
+        {
+            index_s2 = linearSearch(s2, none, 0, p.ind_dim - 1);
+            s2[index_s2] = f1[i];
+        }
+    }
+
+    p.matrix[ind1].swap(s1);
+    p.matrix[ind2].swap(s2);
+}
+
+void crossoverCX(int ind1, int ind2, population &p, int cuts_num)
+{
+    allele none;
+    none.integer = -1;
+    allele start_value;
+    int temp_pos;
+    vector<allele> s1(p.ind_dim, none);
+    vector<allele> s2(p.ind_dim, none);
+    vector<allele> p1 = p.matrix[ind1];
+    vector<allele> p2 = p.matrix[ind2];
+    vector<allele> aux;
+    for(int i = 0; i < p.ind_dim; i++)
+    {
+        // (1) Ciclo sobre o primeiro filho:
+        if(s1[i].integer == -1)
+        {
+            temp_pos = i;
+            start_value = p1[temp_pos];
+            do
+            {
+                s1[temp_pos] = p1[temp_pos];
+                temp_pos = linearSearch(p1, p2[temp_pos], 0, p.ind_dim - 1);
+            } while (p1[temp_pos].integer != start_value.integer);      
+        }
+        // (2) Ciclo sobre o segundo filho:
+        if(s2[i].integer == -1)
+        {
+            temp_pos = i;
+            start_value = p2[temp_pos];
+            do
+            {
+                s2[temp_pos] = p2[temp_pos];
+                temp_pos = linearSearch(p2, p1[temp_pos], 0, p.ind_dim - 1);
+            } while (p2[temp_pos].integer != start_value.integer);      
+        }
+        aux = p1;
+        p1 = p2;
+        p2 = aux;        
+    }
+    p.matrix[ind1] = s1;
+    p.matrix[ind2] = s2;
+}
+
+void mutation(vector<allele> &dna, population &p){
+    vector<int> poll(p.ind_dim - 1);
+    for(size_t i = 0; i < poll.size(); i++) poll[i] = (int) i;
+    for (int i = 0; i < p.ind_dim; i++)
+    {
+        if(randomDouble(0.0, 1.0) <= MUTATION_PROBABILITY)
+            mutateAllele(dna, p, i, poll);
+    }
+}
+
+void mutateAllele(vector<allele> &dna, population &p, int allele_index, vector<int> &poll)
+{
+    int random_index = random() % poll.size();
+    if(random_index >= allele_index)
+        random_index += 1;
+    allele aux = dna[allele_index];
+    dna[allele_index] = dna[random_index];
+    dna[random_index] = aux;
+}
+
+int getElite(nqueens &b, population &p, vector<allele> &elite_place)
+{
+    double best_fitness = fitnessEsc(b, 0, p);
+    int current_best_id = 0;
+    for (int i = 0; i < p.pop_size; i++)
+        {
+            if (fitnessEsc(b, i, p) > best_fitness)
+            {
+                best_fitness = fitnessEsc(b, i, p);
+                current_best_id = i;
+            }
+        }
+    elite_place = p.matrix[current_best_id];
+    return current_best_id;
+}
+
+int getWorst(nqueens &b, population &p)
+{
+    double worst_fitness = fitnessEsc(b, 0, p), current_worst_id = 0;
+    for (int i = 0; i < p.pop_size; i++)
+        {
+            if (fitnessEsc(b, i, p) < worst_fitness)
+            {
+                worst_fitness = fitnessEsc(b, i, p);
+                current_worst_id = i;
+            }
+        }
+    return current_worst_id;
+}
+
+pair<int, int> getPosition(nqueens &b, allele queen, vector<allele> &v)
+{
+    int col = queen.integer;
+    int row = linearSearch(v, queen, 0, v.size() - 1);
     return make_pair(row, col);
 }
 
 int lookDiag(vector<allele> &ind, nqueens &b, allele actual_queen)
 {
     pair<int, int> coord_ind, coord_temp;
-    coord_ind = getPosition(b, actual_queen.integer);
+    coord_ind = getPosition(b, actual_queen, ind);
     //  Obs.: chess_piece é o valor de inteiro do alelo.
     
     //  As diagonais que passam pela rainha que está sendo avaliada são as
@@ -160,7 +526,7 @@ int lookDiag(vector<allele> &ind, nqueens &b, allele actual_queen)
     {   
         if(other_queen.integer == actual_queen.integer)
             continue;
-        coord_temp = getPosition(b, other_queen.integer);
+        coord_temp = getPosition(b, other_queen, ind);
         if(coord_temp.first - coord_temp.second == B_growing || 
                 coord_temp.first + coord_temp.second == B_decreasing)
                 conflicts++;
@@ -171,14 +537,72 @@ int lookDiag(vector<allele> &ind, nqueens &b, allele actual_queen)
 int lookRowAndCol(vector<allele> &ind, nqueens &b, allele actual_queen)
 {
     pair<int, int> coord_ind, coord_temp;
+    coord_ind = getPosition(b, actual_queen, ind);
     int conflicts = 0;
     for(allele &other_queen : ind)
     {   
         if(other_queen.integer == actual_queen.integer)
             continue;
-        coord_temp = getPosition(b, other_queen.integer);
+        coord_temp = getPosition(b, other_queen, ind);
         if(coord_temp.first == coord_ind.first || coord_temp.second == coord_ind.second)
             conflicts++;
     }
     return conflicts;
+}
+
+// Funções para implementação de escalonamento linear:
+//  - Média da função fitness:
+double fitnessAvg(nqueens &b, population &p)
+{
+    double sum = 0.0;
+    for(int i = 0; i < p.pop_size; i++)
+        sum += (double) fitness(b, i, p);
+    sum /= (double) p.pop_size;
+    return sum;
+}
+//  - Valor máximo da função fitness:
+double fitnessMin(nqueens &b, population &p)
+{
+    double m = (double) fitness(b, 0, p);
+    for(int i = 0; i < p.pop_size; i++)
+        m = min(m, (double) fitness(b, i, p)); 
+    return m;
+}
+//  - Valor mínimo da função fitness:
+double fitnessMax(nqueens &b, population &p)
+{
+    double m = (double) fitness(b, 0, p);
+    for(int i = 0; i < p.pop_size; i++)
+        m = max(m, (double) fitness(b, i, p)); 
+    return m;
+}
+//  - Calculo dos valores alpha e beta:
+void calcEsc(nqueens &b, population &p, double c)
+{
+    double f_min = fitnessMin(b, p),
+    f_max = fitnessMax(b, p), f_avg = fitnessAvg(b, p), d;
+
+    if(f_min > (c * f_avg - f_max)/(c - 1.0))
+    {
+        d = f_max - f_avg;
+        p.alpha = f_avg * (c - 1) / d;
+        p.beta = f_avg * (f_max - c * f_avg) / d;
+    }
+    else
+    {
+        d = f_max - f_min;
+        p.alpha = f_avg / d;
+        p.beta = (-f_min) * f_avg / d;
+    }
+
+    if(abs(d) <= 0.00001)
+    {
+        p.alpha = 1.0;
+        p.beta = 0.0;
+    }
+}
+//  - Função fitness com escalonamento:
+double fitnessEsc(nqueens &b, int ind_id, population &p)
+{
+    return max(0.0, p.alpha * ((double) fitness(b, ind_id, p)) + p.beta);
 }
